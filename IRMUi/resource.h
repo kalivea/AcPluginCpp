fd90d50d3dd3e13e61ad08631d4a27a9817dace4
kalivea <passers_by12@hotmail.com>
2025-04-01 17:40:44 +0800

    [bug fix]

--- IRMUi/resource.h
+++ IRMUi/resource.h
@@ -7,6 +7,8 @@
 #define IDC_RADIO_TOP                   101
 #define IDC_RADIO_MID                   102
 #define IDC_RADIO_BOTTOM                103
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP_BAR                  103
 #define IDD_POLAIRMUI                   104
 #define IDC_EDIT_BEAM_INFO              105
 #define IDC_EDIT_BEAM_SGE_NUM           106
@@ -33,7 +35,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         118
 #define _APS_NEXT_SYMED_VALUE           104
